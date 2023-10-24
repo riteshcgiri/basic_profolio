5e1dfe2018c69996dd9cfa67d77f914d05d0fbf4
Ritesh <riteshcgiri@gmail.com>
2023-10-24 21:10:15 +0530

    my basic portfolio - initial commit
    this is my initial commit

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+console.log('working');
+
+// let tabLinks = document.querySelectorAll('.tabs__links');
+// console.log(tabLinks);
+// let tabContents = document.querySelectorAll('.tabs-content');
+// let tabName;
+// tabLinks.forEach((tabLink)=>{
+//     tabLink.addEventListener('click', (e)=>{
+//         // tabName  = e.target.textContent.toLowerCase()
+//         // openTab(tabName)
+//         tabLinks.forEach(e => e.classList.remove('active-link'));
+//         tabContents.forEach((tabContent)=>{
+//             tabContent.classList.remove('active-tab');
+//         })
+//         event.currentTarget.classList.add('acitve-link');
+        
+//     })
+// })
+
+
+
